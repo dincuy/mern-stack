0730d62479527397ebeabe2fbbce7f8b464bb46f
Dincuy <66787508+dincuy@users.noreply.github.com>
2024-02-20 04:35:47 +0000

    delete product

--- simple-crud-app/index.js
+++ simple-crud-app/index.js
@@ -9,12 +9,69 @@ app.get("/", function (req, res) {
   res.send("hellow cuy");
 });
 
+app.get("/api/products", async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.get("/api/products/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const products = await Product.findById(id);
+    
+    if (!product) {
+      return res.status(404).json({ message: "Produk tidak ditemukan" });
+    }
+    
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
 app.post("/api/products", async (req, res) => {
   try {
     const product = await Product.create(req.body);
     res.status(200).json(product);
   } catch (error) {
-    res.status(500).json({ message: error });
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.put("/api/products/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body);
+
+    if (!product) {
+      return res.status(404).json({ message: "Produk tidak ditemukan" });
+    }
+
+    const updatedProduct = await Product.findById(id);
+    res.status(200).json(updatedProduct);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// delete product
+app.delete("/api/products/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const product = await Product.findByIdAndDelete(id);
+
+    if (!product) {
+      return res.status(404).json({ message: "Produk tidak ditemukan" });
+    }
+
+    res.status(200).json({ message: `Produk dengan id ${id} sukses dihapus` });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
   }
 });
 
